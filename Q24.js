1d165651c7fe2fdf6c3c5c07c6f293d3c2f5ff3e
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-15 20:28:05 +0500

    Add files via upload

---
+++ Q24.js
@@ -0,0 +1,13 @@
+//Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+//• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+//• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+// Create a variable called alien_color
+var alien_color = 'green'; // Assign it a value of 'green', 'yellow', or 'red'.
+//• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points.");
+}
+// Version that fails the if test (no output)
+if (alien_color === 'blue') {
+    console.log("Congratulations! You just earned 5 points.");
+}
