ae4aa990432c1e9863962263bf3dccb0e526260c
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-16 20:27:58 +0500

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,5 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+//  the array of dinner guests
+var dinnerGuests = ["Mahnoor", "Areba", "Laiba", "Kashaf", "Alena"];
+//  the number of dinner guests
+console.log("You are inviting ".concat(dinnerGuests.length, " people to dinner."));
---
+++ Q19.ts
@@ -0,0 +1,7 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+//  the array of dinner guests
+const dinnerGuests: string[] = ["Mahnoor", "Areba", "Laiba", "Kashaf", "Alena"];
+
+//  the number of dinner guests
+console.log(`You are inviting ${dinnerGuests.length} people to dinner.`);
