1ece23165a7e842386e849f564eb5b4e853b1dce
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-16 20:42:35 +0500

    Add files via upload

---
+++ Q7.js
@@ -0,0 +1,13 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.//
+// Addition operation
+var additionResult = 5 + 3;
+console.log("Addition Result: ".concat(additionResult));
+// Subtraction operation
+var subtractionResult = 10 - 2;
+console.log("Subtraction Result: ".concat(subtractionResult));
+// Multiplication operation
+var multiplicationResult = 4 * 2;
+console.log("Multiplication Result: ".concat(multiplicationResult));
+// Division operation
+var divisionResult = 64 / 8;
+console.log("Division Result: ".concat(divisionResult));
---
+++ Q7.ts
@@ -0,0 +1,18 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.//
+
+
+// Addition operation
+const additionResult: number = 5 + 3;
+console.log(`Addition Result: ${additionResult}`);
+
+// Subtraction operation
+const subtractionResult: number = 10 - 2;
+console.log(`Subtraction Result: ${subtractionResult}`);
+
+// Multiplication operation
+const multiplicationResult: number = 4 * 2;
+console.log(`Multiplication Result: ${multiplicationResult}`);
+
+// Division operation
+const divisionResult: number = 64 / 8;
+console.log(`Division Result: ${divisionResult}`);
