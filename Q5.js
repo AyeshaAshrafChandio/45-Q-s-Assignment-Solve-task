733c966c239865e4bdf0a515f2bb4a2685ef12fc
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-16 20:29:16 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,8 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.//
+// Define the famous person and the quote
+var famous_person = "Alllama iqbal";
+var quote = "Islam is itself destiny and will not suffer destiny.";
+// Compose the message
+var message = "".concat(famous_person, " once said, \"").concat(quote, "\"");
+// Print the message
+console.log(message);
---
+++ Q5.ts
@@ -0,0 +1,12 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.//
+
+// Define the famous person and the quote
+const famous_person: string = "Alllama iqbal";
+const quote: string = "Islam is itself destiny and will not suffer destiny.";
+
+// Compose the message
+const message: string = `${famous_person} once said, "${quote}"`;
+
+// Print the message
+console.log(message);
+
