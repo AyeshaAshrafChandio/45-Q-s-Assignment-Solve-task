f7ea071a3d5c95e419ca0e7e211c8585599d03bd
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-16 20:27:05 +0500

    Add files via upload

---
+++ Q23.ts
@@ -0,0 +1,48 @@
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+
+//console.log("Is car == 'subaru'? I predict True.")
+
+//console.log(car == 'subaru')
+
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False
+
+let car: string = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+
+console.log("Is car == 'ford'? I predict False.");
+console.log(car == 'ford');
+
+
+console.log("Is car != 'toyota'? I predict True.");
+console.log(car != 'toyota');
+
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+
+console.log("Is car !== 'ford'? I predict True.");
+console.log(car !== 'ford');
+
+console.log("Is car !== 'subaru'? I predict False.");
+console.log(car !== 'subaru');
+
+
+console.log("Is car.length < 6? I predict False.");
+console.log(car.length < 6);
+
+
+console.log("Is car.length > 4? I predict True.");
+console.log(car.length > 4);
+
+
+console.log("Is car.toLowerCase() == 'subaru'? I predict True.");
+console.log(car.toLowerCase() == 'subaru');
+
+
+console.log("Is car.toUpperCase() === 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() === 'SUBARU');
