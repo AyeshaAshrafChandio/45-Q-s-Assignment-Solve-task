fd383d9e64781c90d31492a3e395606cb008bdcb
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-16 20:32:44 +0500

    Add files via upload

---
+++ Q14.ts
@@ -0,0 +1,11 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner
+
+
+  //  guests to invite to dinner
+const guestList: string[] = ["muqadas", "Mahnoor", " kinza"];
+
+//  array and invitation messages
+guestList.forEach((guest) => {
+    console.log(`Dear ${guest},\n\nYou are  invited to dinner. Please join us for a shining evening.\n\nBest regards,\n[Mehak]`);
+});
+
