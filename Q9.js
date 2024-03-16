4b1a1fb0d52b72c046c68a1f5baad1a88e68d87c
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-16 20:30:34 +0500

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,7 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.//
+// Define your favorite number
+var favoriteNumber = 6;
+// Create a message revealing your favorite number
+var message = "My favorite number is ".concat(favoriteNumber, ".");
+// Print the message
+console.log(message);
---
+++ Q9.ts
@@ -0,0 +1,12 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.//
+
+
+
+// Define your favorite number
+const favoriteNumber: number = 6;
+
+// Create a message revealing your favorite number
+const message: string = `My favorite number is ${favoriteNumber}.`;
+
+// Print the message
+console.log(message);
