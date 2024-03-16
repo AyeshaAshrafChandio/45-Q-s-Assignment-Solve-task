11a5eb39a15bcaf1e0a2a8f297332dc73f4c4c76
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-16 20:40:20 +0500

    Add files via upload

---
+++ Q11.js
@@ -0,0 +1,6 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var names = ['zuhaib', 'rimsha', 'mahnoor', 'saba', 'sana'];
+// Loop through the names array and print each name
+names.forEach(function (name) {
+    console.log(name);
+});
---
+++ Q11.ts
@@ -0,0 +1,8 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+
+const names: string[] =['zuhaib', 'rimsha', 'mahnoor', 'saba','sana'];
+// Loop through the names array and print each name
+names.forEach(name => {
+    console.log(name);
+});
