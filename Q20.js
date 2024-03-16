9be9d1c8bb4a6979ae3cf75be785dfc7a37f43b0
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-16 20:49:26 +0500

    Add files via upload

---
+++ Q20.js
@@ -0,0 +1,6 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+// Define an array to store mountains and rivers in Pakistan
+var pakistanLocations = ["K2", "Nanga Parbat", "Himalayas", "Indus River", "Jhelum River", "Chenab River"];
+// Print the array
+console.log("Mountains and rivers in Pakistan:");
+pakistanLocations.forEach(function (location) { return console.log(location); });
---
+++ Q20.ts
@@ -0,0 +1,9 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+
+// Define an array to store mountains and rivers in Pakistan
+const pakistanLocations: string[] = ["K2", "Nanga Parbat", "Himalayas", "Indus River", "Jhelum River", "Chenab River"];
+
+// Print the array
+console.log("Mountains and rivers in Pakistan:");
+pakistanLocations.forEach(location => console.log(location));
