6cd72810ed924fef5e014635306265e37ba1966a
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-16 20:28:38 +0500

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,4 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks://
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”//
+var name1 = ("Allama Iqbal said");
+console.log("".concat(name1, "\u201Cdont look at backward look farword.\u201D"));
---
+++ Q4.ts
@@ -0,0 +1,6 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks://
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”//
+
+
+let name1:string = ("Allama Iqbal said")
+console.log(`${name1}“dont look at backward look farword.”`)       
