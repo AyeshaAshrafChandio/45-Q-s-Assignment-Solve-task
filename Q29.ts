b43945aed1a1ca1ab6fe6050830b40d703c776a9
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-16 20:29:49 +0500

    Add files via upload

---
+++ Q29.ts
@@ -0,0 +1,25 @@
+//Favorite Fruit: Make a array of your favorite fruits, and then write a series of indepenent if statements //that check for certain fruits in your array.
+//• Make a array of your three favorite fruits and call it favorite_fruits.
+
+//• Write five if statements. Each should check whether a certain kind of fruit is in your arr//ay. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+
+
+// Define an array of favorite fruits
+const favorite_fruits: string[] = ["watermellon", "grapes", "orange"];
+
+// Function to check if a fruit is one of the favorite fruits
+function checkForFruit(fruit: string): void {
+    if (favorite_fruits.indexOf(fruit) !== -1) {
+        console.log(`I really like ${fruit}s!`);
+    } else {
+        console.log(`I don't seem to like ${fruit}s.`);
+    }
+}
+
+// Check for certain fruits using independent if statements
+checkForFruit("watermellon");
+checkForFruit("grapes");
+checkForFruit("orange");
+checkForFruit("peach");
+checkForFruit("gava");
