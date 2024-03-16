c1ea83d4e3b8de166512afd46a16765f2ae3c861
AyeshaAshrafChandio <162025650+AyeshaAshrafChandio@users.noreply.github.com>
2024-03-17 01:32:20 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,7 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.//
+
+
+//author: [LISA]
+//date: [Monday, Feburary 25, 2024]
+//this programe will run simple code just like Good Moorning
+console.log('Good Moorning simple programe');
---
+++ programe2.js
@@ -0,0 +1,4 @@
+//my name is lisa
+//Dated:21/o2/2024
+//this programe will multipication
+console.log(5 * 2);
